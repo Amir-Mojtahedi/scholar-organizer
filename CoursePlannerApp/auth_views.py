61b98db990612d98e277fa97fe40c72f81f5f648
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-07 11:04:09 -0400

    Removed login functionality in order to seperate different aspects of the project

--- CoursePlannerApp/auth_views.py
+++ CoursePlannerApp/auth_views.py
@@ -18,26 +18,3 @@ def signup():
                 user=User(form.email.data,hash,form.name.data)
                 get_db().add_user(user)
     return render_template('signup.html',form=form)
-
-@bp.route('/login/',methods=['GET','POST'])
-def login():
-    form=LoginForm()
-    if request.method=='POST':
-        if form.validate_on_submit():
-            user=get_db().get_user(form.email.data)
-            if user:
-                if check_password_hash(user.password,form.password.data):
-                    login_user(user,form.remember_me.data)
-                else:
-                    flash("Cannot Login")
-            else:
-                flash('Cannot Login')
-        else:
-            flash('Cannot Login')
-    return render_template('login.html',form=form)
-
-@bp.route('/logout/')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
