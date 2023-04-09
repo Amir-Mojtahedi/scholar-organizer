15548f58b2c5066ef30091c8135c18cad7696cb8
dxxxxy <introsdream@gmail.com>
2023-04-09 15:19:26 -0400

    refactor: mode pep 8 + error handling

--- CoursePlannerApp/competency_api.py
+++ CoursePlannerApp/competency_api.py
@@ -1,12 +1,21 @@
+import oracledb
 from flask import Blueprint, jsonify
+from werkzeug.local import LocalProxy
+
 from .dbmanager import get_db
 
-bp = Blueprint('competency_api', __name__, url_prefix='/api/competencies/')
+bp = Blueprint("competency_api", __name__, url_prefix="/api/competencies/")
 
-#Competency Page 
-@bp.route("/", methods=['GET', 'POST'])
+dtb = LocalProxy(get_db)
+
+
+@bp.route("/", methods=["GET", "POST"])
 def competencies_api():
-    competencies = get_db().get_competencies()
-    #Return * competencies in json format
+    try:
+        competencies = dtb.get_competencies()
+    except oracledb.Error as e:
+        return jsonify({"error": str(e)}), 500
+
+    # Return * competencies in json format
     json = [competency.__dict__ for competency in competencies]
     return jsonify(json)
