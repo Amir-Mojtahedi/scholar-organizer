4add23933dfb24ef0a1ece77dcaedd30507bd484
dxxxxy <introsdream@gmail.com>
2023-04-09 15:21:02 -0400

    refactor: objects pep 8

--- CoursePlannerApp/objects/user.py
+++ CoursePlannerApp/objects/user.py
@@ -1,4 +1,8 @@
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, StringField, BooleanField
+from wtforms.validators import DataRequired
+
 
 class User(UserMixin):
     def __init__(self, email, password, name, id=None):
@@ -10,21 +14,20 @@ class User(UserMixin):
             raise TypeError("Name must be a string")
         if id and not isinstance(id, int):
             raise TypeError("Id must be an integer")
+
         self.email = email
         self.password = password
         self.name = name
         self.id = id
 
-from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField, BooleanField
-from wtforms.validators import DataRequired
 
 class SignupForm(FlaskForm):
-    email=EmailField('email',validators=[DataRequired()])
-    password=PasswordField('password',validators=[DataRequired()])
-    name=StringField('name',validators=[DataRequired()])
+    email = EmailField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    name = StringField("name", validators=[DataRequired()])
+
 
 class LoginForm(FlaskForm):
-    email=EmailField('email',validators=[DataRequired()])
-    password=PasswordField('password',validators=[DataRequired()])
-    remember_me=BooleanField('remember me')
+    email = EmailField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    remember_me = BooleanField("remember me")
