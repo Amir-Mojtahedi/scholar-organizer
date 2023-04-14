6695f9c7c054bc7867be6e7b38a1038755b5fe28
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-13 23:27:57 -0400

    Changed the URL prefix to start from /display

--- CoursePlannerApp/views/display_views.py
+++ CoursePlannerApp/views/display_views.py
@@ -2,7 +2,7 @@ import oracledb
 from flask import Blueprint, flash, render_template, request
 from werkzeug.local import LocalProxy
 from CoursePlannerApp.dbmanager import get_db
-bp = Blueprint('display', __name__, url_prefix='/')
+bp = Blueprint('display', __name__, url_prefix='/display')
 
 dtb = LocalProxy(get_db)
 
