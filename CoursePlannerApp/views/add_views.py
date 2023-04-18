98534d70c580ad23fe6ff224be4594b88925774d
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-18 13:55:39 -0400

    Moved all the add options to another view to make things better

--- CoursePlannerApp/views/add_views.py
+++ CoursePlannerApp/views/add_views.py
@@ -2,13 +2,46 @@ import oracledb
 from flask import Blueprint, flash, render_template, request
 from werkzeug.local import LocalProxy
 from CoursePlannerApp.dbmanager import get_db
-bp = Blueprint('add', __name__, url_prefix='/add')
+from flask_login import login_required
+
+from CoursePlannerApp.objects.course import CourseForm
+from CoursePlannerApp.objects.competency import CompetencyForm
+from CoursePlannerApp.objects.domain import DomainForm
+from CoursePlannerApp.objects.term import TermForm
+from CoursePlannerApp.objects.element import ElementForm
+
+bp = Blueprint('add', __name__, url_prefix='/')
 
 dtb = LocalProxy(get_db)
 
-#Add Page
-@bp.route("/")
-def display():
-    return render_template('Add/addEntities.html')
+@bp.route("/add-course/")
+@login_required
+def add_course():
+    form=CourseForm()
+    return render_template('/Add/addCourse.html',form=form)
+
+@bp.route("/add-competency/")
+@login_required
+def add_competency():
+    form=CompetencyForm()
+    return render_template('/Add/addCompetency.html',form=form)
+
+@bp.route("/add-domain/")
+@login_required
+def add_domain():
+    form=DomainForm()
+    return render_template('/Add/addDomain.html',form=form)
+
+@bp.route("/add-element-of-competency/")
+@login_required
+def add_element_competency():
+    form=ElementForm()
+    return render_template('/Add/addElement.html',form=form)
+
+@bp.route("/add-term/")
+@login_required
+def add_term():
+    form=TermForm()
+    return render_template('/Add/addTerm.html',form=form)
 
 
