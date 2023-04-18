f285b34fd6cbb0782036e1e88bd55f31fe918713
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-04-18 13:51:39 -0400

    Added all the buttons to add brand new courses, terms, domains and etc

--- CoursePlannerApp/views/home_views.py
+++ CoursePlannerApp/views/home_views.py
@@ -2,7 +2,13 @@ from flask import Blueprint, render_template, flash, render_template, request
 from werkzeug.local import LocalProxy
 from CoursePlannerApp.dbmanager import get_db
 from flask_login import login_required
+
 from CoursePlannerApp.objects.course import CourseForm
+from CoursePlannerApp.objects.competency import CompetencyForm
+from CoursePlannerApp.objects.domain import DomainForm
+from CoursePlannerApp.objects.term import TermForm
+from CoursePlannerApp.objects.element import ElementForm
+
 import oracledb
 
 dtb = LocalProxy(get_db)
@@ -43,9 +49,35 @@ def list_elements(competency_id):
             flash('There is no competency in the database')            
     return render_template('elements.html', elements = elements)
 
-@bp.route("/Add-Course/")
+@bp.route("/add-course/")
 @login_required
 def add_course():
     form=CourseForm()
     return render_template('/Add/addCourse.html',form=form)
+
+@bp.route("/add-competency/")
+@login_required
+def add_competency():
+    form=CompetencyForm()
+    return render_template('/Add/addCompetency.html',form=form)
+
+@bp.route("/add-domain/")
+@login_required
+def add_domain():
+    form=DomainForm()
+    return render_template('/Add/addDomain.html',form=form)
+
+@bp.route("/add-element-of-competency/")
+@login_required
+def add_element_competency():
+    form=ElementForm()
+    return render_template('/Add/addElement.html',form=form)
+
+@bp.route("/add-term/")
+@login_required
+def add_term():
+    form=TermForm()
+    return render_template('/Add/addTerm.html',form=form)
+
+
 
