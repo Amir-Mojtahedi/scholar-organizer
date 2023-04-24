22035f5816442d574f359c1d6c21ca804918e5bb
dxxxxy <introsdream@gmail.com>
2023-04-23 23:18:34 -0400

    feat: show groups for level 2 admins

--- CoursePlannerApp/views/users_views.py
+++ CoursePlannerApp/views/users_views.py
@@ -1,5 +1,5 @@
 import oracledb
-from flask import Blueprint, render_template, flash, request
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from werkzeug.local import LocalProxy
 
@@ -15,7 +15,7 @@ dtb = LocalProxy(get_db)
 def index():
     # get user
     user = current_user
-    manages = user.group_id == 1 or user.group_id == 2  # admin_user_gp or admin_gp (respectively)
+    level = user.group_id
 
     # get all users
     try:
@@ -24,12 +24,20 @@ def index():
         flash("There was an error retrieving the users from the database")
         users = []
 
-    # get all groups
-    try:
-        groups = dtb.get_groups()
-    except oracledb.Error:
-        flash("There was an error retrieving the groups from the database")
-        groups = []
+    if level == 0 or level == 1:  # members and admin_user_gp can see members
+        users = [u for u in users if u.group_id == 0]
+        return render_template("users.html", manages=level == 1, groups=[], users=users)
 
-    if request.method == "GET":
-        return render_template("users.html", manages=manages, groups=groups, users=users)
+    elif level == 2:  # admin_gp can see all users and groups
+        # get all groups
+        try:
+            groups = dtb.get_groups()
+        except oracledb.Error:
+            flash("There was an error retrieving the groups from the database")
+            groups = []
+
+        # insert a group into each user
+        for u in users:
+            u.group = next(g for g in groups if g.id == u.group_id)
+
+        return render_template("users.html", manages=True, groups=groups, users=users)
