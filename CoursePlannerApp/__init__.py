c8637018f285d44ad329ff171f70b5a40311f1e3
dxxxxy <introsdream@gmail.com>
2023-05-03 17:07:37 -0400

    refactor: __init__.py

--- CoursePlannerApp/__init__.py
+++ CoursePlannerApp/__init__.py
@@ -1,28 +1,27 @@
-import secrets
 import os
+import secrets
 
-from flask import Flask, render_template, current_app, redirect, url_for, flash
-from flask_login import LoginManager, current_user
-
-from CoursePlannerApp.views.auth_views import bp as auth_bp
-from CoursePlannerApp.views.home_views import bp as home_bp
-from CoursePlannerApp.views.display_views import bp as display_bp
-from CoursePlannerApp.views.competency_views import bp as competency_bp
-from CoursePlannerApp.views.course_views import bp as course_bp
-from CoursePlannerApp.views.domain_views import bp as domain_bp
-from CoursePlannerApp.views.element_views import bp as element_bp
-from CoursePlannerApp.views.term_views import bp as term_bp
-from CoursePlannerApp.views.groups_views import bp as groups_bp
-from CoursePlannerApp.views.users_views import bp as users_bp
+from flask import Flask, render_template
+from flask_login import LoginManager
 
 from CoursePlannerApp.apis.domains_api import bp as domains_api
-
+from CoursePlannerApp.apis.terms_api import bp as terms_api
+from CoursePlannerApp.views.auth_views import bp as auth_bp
+from CoursePlannerApp.views.competency_views import bp as competency_bp
+from CoursePlannerApp.views.course_views import bp as course_bp
+from CoursePlannerApp.views.display_views import bp as display_bp
+from CoursePlannerApp.views.domain_views import bp as domain_bp
+from CoursePlannerApp.views.element_views import bp as element_bp
+from CoursePlannerApp.views.groups_views import bp as groups_bp
+from CoursePlannerApp.views.home_views import bp as home_bp
+from CoursePlannerApp.views.term_views import bp as term_bp
+from CoursePlannerApp.views.users_views import bp as users_bp
 from .dbmanager import close_db, init_db_command, get_db
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY=secrets.token_urlsafe(32),IMAGE_PATH=os.path.join(app.instance_path,'images'))
+    app.config.from_mapping(SECRET_KEY=secrets.token_urlsafe(32), IMAGE_PATH=os.path.join(app.instance_path, 'images'))
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
@@ -40,17 +39,15 @@ def create_app(test_config=None):
     @app.errorhandler(404)
     def page_not_found(_):
         return render_template("custom404.html"), 404
-    
+
     os.makedirs(app.instance_path, exist_ok=True)
     os.makedirs(app.config['IMAGE_PATH'], exist_ok=True)
-
 
     init_app(app)
     return app
 
 
 def init_app(app):
-    # DbManager
     app.cli.add_command(init_db_command)
     app.teardown_appcontext(close_db)
 
@@ -64,7 +61,7 @@ def init_app(app):
     app.register_blueprint(term_bp)
 
     app.register_blueprint(groups_bp)
-
     app.register_blueprint(users_bp)
 
     app.register_blueprint(domains_api)
+    app.register_blueprint(terms_api)
