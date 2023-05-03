c731158d2b7c9ed424a3fb54f094ee6124eb3886
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-05-03 10:21:40 -0400

    Fixed a minor issue

--- CoursePlannerApp/views/course_views.py
+++ CoursePlannerApp/views/course_views.py
@@ -48,15 +48,8 @@ def add_element_for_course(course_id):
         if form.validate_on_submit():
             try:
                 dtb.add_element_course_bridging(int(form.id.data),course_id,int(form.element_hours.data))
-                course=dtb.get_specific_course(course_id)
-                total_hours=(course.lab_hours + course.theory_hours) * 15
-                current_hours=dtb.get_sum_hours(course_id)
-                diff=total_hours-current_hours
-                flash("Element was added successfully")
-                if(diff<0):
-                    flash(f'You must remove {diff*-1} to match {total_hours} of {course.name}')
-                elif(diff>0):
-                    flash(f'You must add {diff} hours to match {total_hours} hours of {course.id} {course.name}')
+                flash("Element was created successfully.")
+                hour_validator(course_id)
                 return redirect(url_for('courses.list_competencies',course_id=course_id))
             except oracledb.IntegrityError as e:
                 error_obj, = e.args #To acces code error 
@@ -90,8 +83,9 @@ def create_course():
                                form.work_hours.data)
             try:
                 dtb.add_course(newCourse)
-                flash("Course has been added")    
-                return redirect(url_for('courses.get_courses'))
+                flash("Course has been added")
+                hour_validator(form.id.data)    
+                return redirect(url_for('courses.list_competencies',course_id=form.id.data))
             
             except oracledb.IntegrityError as e:
                 error_obj, = e.args #To acces code error 
@@ -169,18 +163,21 @@ def delete(course_id):
 @login_required
 def delete_element_for_course(course_id,element_id):
     try:
-        course=dtb.get_specific_course(course_id)
-        total_hours=(course.lab_hours + course.theory_hours) * 15    
         dtb.delete_element_course_bridging(element_id,course_id)
-        current_hours=dtb.get_sum_hours(course_id)
-        diff=total_hours-current_hours
         flash("Element deleted successfully")
-        if(diff<0):
-            flash(f'You must remove {diff*-1} to match {total_hours} of {course.name}')
-        elif(diff>0):
-            flash(f'You must add {diff} hours to match {total_hours} hours of {course.id} {course.name}')
+        hour_validator(course_id)
     except Exception as e:
         flash("Could not access the record")
         flash("Error: " + str(e))
         return redirect(url_for('courses.list_competencies',course_id=course_id))
     return redirect(url_for('courses.list_competencies',course_id=course_id))
+
+def hour_validator(course_id):
+    course=dtb.get_specific_course(course_id)
+    total_hours=(course.lab_hours + course.theory_hours) * 15
+    current_hours=dtb.get_sum_hours(course_id)
+    diff=total_hours-current_hours
+    if(diff<0):
+        flash(f'You must remove {diff*-1} to match {total_hours} of {course.name}')
+    elif(diff>0):
+        flash(f'You must add {diff} hours to match {total_hours} hours of {course.id} {course.name}')
