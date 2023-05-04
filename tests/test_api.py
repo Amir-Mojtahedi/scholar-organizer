bb0969c8b7bc6a25c7966b9bc83a944ac7ba7113
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-05-04 09:17:26 -0400

    Finished test apis for terms

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,7 @@ from CoursePlannerApp import create_app
 class TestForAPI(flask_unittest.ClientTestCase):
     app = create_app()
 
+    # --------------- START DOMAIN CRUD TEST ---------------
     def test_get_domains(self, client):
         resp = client.get('/api/v1/domains')
         self.assertEqual(resp.status_code, 200)
@@ -21,7 +22,6 @@ class TestForAPI(flask_unittest.ClientTestCase):
         self.assertIsNotNone(json["id"])
         self.assertIsNotNone(json["name"])
         self.assertIsNotNone(json["description"])
-
 
     def test_add_domain(self, client):
         resp = client.get('/api/v1/domains')
@@ -48,3 +48,59 @@ class TestForAPI(flask_unittest.ClientTestCase):
         domain = resp.json
         resp = client.delete('/api/v1/domains/4', json=domain)
         self.assertEqual(resp.status_code, 204)
+    # --------------- END DOMAIN CRUD TEST ---------------
+
+    # --------------- START TERM CRUD TEST ---------------
+    def test_get_terms(self, client):
+        resp = client.get('/api/v1/terms')
+        self.assertEqual(resp.status_code, 200)
+        json = resp.json
+        self.assertIsNotNone(json)
+        self.assertIsNotNone(json['count'])
+        self.assertIsNotNone(json['results'])
+        self.assertIsNone(json['next'])
+        self.assertIsNone(json['prev'])
+
+    def test_get_term(self,client):
+        resp = client.get('/api/v1/terms/1')
+        self.assertEqual(resp.status_code, 200)
+        json = resp.json
+        self.assertIsNotNone(json)
+        self.assertIsNotNone(json["id"])
+        self.assertIsNotNone(json["name"])
+
+    def test_add_term(self, client):
+        resp = client.get('/api/v1/terms')
+        self.assertEqual(resp.status_code, 200)
+        term = resp.json['results'][0]
+        term['id'] = 7
+        term['name'] = 'FALL'
+        resp = client.post('/api/v1/terms', json=term)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_update_term(self, client):
+        resp = client.get('/api/v1/terms/7')
+        self.assertEqual(resp.status_code, 200)
+        term = resp.json
+        term['id'] = 7
+        term['name'] = 'FALL'
+        resp = client.patch('/api/v1/terms/7', json=term)
+        self.assertEqual(resp.status_code, 204)
+
+    def test_delete_term(self, client):
+        resp = client.get('/api/v1/terms/7')
+        self.assertEqual(resp.status_code, 200)
+        term = resp.json
+        resp = client.delete('/api/v1/terms/7', json=term)
+        self.assertEqual(resp.status_code, 204)
+    # --------------- END TERM CRUD TEST ---------------
+
+    # --------------- START COMPETENCY CRUD TEST ---------------
+    # --------------- END COMPETENCY CRUD TEST ---------------
+
+    # --------------- START ELEMENT OF COMPETENCY CRUD TEST ---------------
+    # --------------- END ELEMENT OF COMPETENCY CRUD TEST ---------------
+
+    # --------------- START COURSE CRUD TEST ---------------
+    # --------------- END COURSE CRUD TEST ---------------
+
